51bf2cf7a6908dbcaabb93ab9659762863a8030f
Kem Guianalan <guianalankem@gmail.com>
2024-05-24 08:14:11 +0800

    chore: Update .gitignore and settings.py
    Update .gitignore to include .env file and remove trailing newline.
    Update settings.py to use environment variables for SECRET_KEY and DEBUG, and switch to PostgreSQL database.

--- alumniwebsite/settings.py
+++ alumniwebsite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)^)5i#ocvur)t4*d5b9e+fc&5z7+t5)h8w84ue2q2#$!=0k)*5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +79,13 @@ WSGI_APPLICATION = 'alumniwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'sslmode': config('DB_SSLMODE'),
     }
 }
 
