b169647099f5b29670e39ea4cf57da5b673e32ed
Kem Guianalan <guianalankem@gmail.com>
2024-05-24 09:08:53 +0800

    chore: Update ALLOWED_HOSTS and DIRS in settings.py
    Update ALLOWED_HOSTS to use environment variable for configuration.
    Update DIRS in TEMPLATES settings to include 'templates' directory.

--- alumniwebsite/settings.py
+++ alumniwebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'alumniwebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
