f866a26d60793c9528268939d95737587071bb03
Kem Guianalan <guianalankem@gmail.com>
2024-06-18 05:34:14 +0800

    chore: Add user-related files and templates

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.login , name='login'),
+    path('signup/', views.register , name='register'),
+    path('faculty/', views.faculty , name='faculty')
+]
