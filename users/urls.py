8c1c1865aa6448d670a4dc2e08cec0b9d51be83d
Kem Guianalan <guianalankem@gmail.com>
2024-06-18 15:25:41 +0800

    chore: Update login and signup templates and styles

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
+    path('portal/', views.portal , name='portal'),
     path('login/', views.login , name='login'),
     path('signup/', views.register , name='register'),
     path('faculty/', views.faculty , name='faculty')
