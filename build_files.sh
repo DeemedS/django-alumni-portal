3b7dd9fe050f1fcc761898fe24a44ed2fa1f6859
Kem Guianalan <guianalankem@gmail.com>
2024-08-20 06:35:16 +0800

    Update build configuration and dependencies

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
