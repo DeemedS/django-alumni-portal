eeee1195e41e6f147a1f1b7e5838d1b95da3b122
Kem Guianalan <guianalankem@gmail.com>
2024-08-30 15:34:40 +0800

    Handle image loading errors and display default image

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+// Load default image if image not found
+document.querySelectorAll("img").forEach((img) => {
+    img.onerror = function () {
+      console.error(`Failed to load image: ${this.src}`);
+      this.src = "{% static 'images/default_image.png' %}";
+    };
+  });
+  
