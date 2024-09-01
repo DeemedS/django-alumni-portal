42998c74246270bce605726e85dafa5d015050d6
Kem Guianalan <guianalankem@gmail.com>
2024-09-01 11:29:59 +0800

    Add authentication URLs and integrate djangorestframework-simplejwt

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 
 app_name = 'api'
 
 urlpatterns = [
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    
     path('filtered-events/', views.FilteredEventsAPIView.as_view(), name='filtered_events_api'),
     path('filtered-articles/', views.FilteredArticlesAPIView.as_view(), name='filtered_articles_api'),
 ]
