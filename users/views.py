2da3027309dada32d45de7f6bc88fcde34f8208f
Kem Guianalan <guianalankem@gmail.com>
2024-09-01 11:35:27 +0800

    add template for user dashboard, and protect the view using djangorestframework-simplejwt

--- users/views.py
+++ users/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import JsonResponse
 
-# Create your views here.
-def portal(request):
-    return render(request, 'portal.html')
+def user_dashboard(request):
 
-def login(request):
-    return render(request, 'login.html')
+    access_token = request.COOKIES.get('access_token')
+    refresh_token = request.COOKIES.get('refresh_token')
 
-def register(request):
-    return render(request, 'signup.html')
+    if access_token:
+        api_url = request.build_absolute_uri(reverse('api:token_verify'))
+        data = {'token': access_token}
+        response = requests.post(api_url, data=data)
 
-def faculty(request):
-    return render(request, 'faculty.html')
+        if response.status_code == 200:
+            return render(request, 'user_dashboard.html')
+        
+        elif response.status_code == 401 and refresh_token:
+
+            refresh_url = request.build_absolute_uri(reverse('api:token_refresh'))
+            refresh_response = requests.post(refresh_url, data={'refresh': refresh_token})
+
+            if refresh_response.status_code == 200:
+                new_tokens = refresh_response.json()
+                access_token = new_tokens.get('access')
+                response = redirect('/dashboard/')
+                response.set_cookie('access_token', access_token, httponly=True)
+                return response
+            
+            else:
+                return redirect('/login/')
+        else:
+            return redirect('/login/')
+    else:
+        return redirect('/login/')
+
