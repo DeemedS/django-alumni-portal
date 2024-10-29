70bf2643471f0c24dfe5e2cc8bb4d3617d1f3ef6
Kem Guianalan <guianalankem@gmail.com>
2024-10-30 00:35:14 +0800

    add delay for replacing failed images

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,12 @@
 // Load default image if image not found
 document.addEventListener('DOMContentLoaded', function() {
-document.querySelectorAll("img").forEach((img) => {
-    img.onerror = function () {
-      console.error(`Failed to load image: ${this.src}`);
-      this.src = '/static/images/default_image.png';
-    };
-  });
+  setTimeout(() => {
+    document.querySelectorAll("img").forEach((img) => {
+      img.onerror = function () {
+        console.error(`Failed to load image: ${this.src}`);
+        this.src = '/static/images/default_image.png';
+      };
+    });
+  }, 2000);
 });
   
