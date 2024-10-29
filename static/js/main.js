a9b6b774ceab03cd967bb78f26fddc1290eed3a5
Kem Guianalan <guianalankem@gmail.com>
2024-10-30 00:56:43 +0800

    Use the Load Event for Images

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,21 @@
 // Load default image if image not found
 
-function setImageErrorHandling() {
-  document.querySelectorAll("img").forEach((img) => {
-    img.onerror = function () {
-      console.error(`Failed to load image: ${this.src}`);
-      this.src = '/static/images/default_image.png';
-    };
-  });
-}
+document.querySelectorAll("img").forEach((img) => {
+  img.onerror = function () {
+    console.error(`Failed to load image: ${this.src}`);
+    this.src = '/static/images/default_image.png';
+  };
+});
 
-setImageErrorHandling();
 
 document.addEventListener('DOMContentLoaded', function() {
-  const observerTarget = document.body; 
-  
-  if (observerTarget instanceof Node) {
-    const observer = new MutationObserver(setImageErrorHandling);
-    observer.observe(observerTarget, {
-      childList: true,  // Observe direct children
-      subtree: true,    // Observe all descendants
-    });
-  }
+  document.querySelectorAll("img").forEach((img) => {
+    if (!img.complete) {
+      img.onerror = function () {
+        console.error(`Failed to load image: ${this.src}`);
+        this.src = '/static/images/default_image.png';
+      };
+    }
+  });
 });
+  
