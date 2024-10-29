284488d42a847e865ba80898473ab3be6e2a16f6
Kem Guianalan <guianalankem@gmail.com>
2024-10-30 00:51:54 +0800

    use MutationObserver for failed images.

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,21 @@
 // Load default image if image not found
-document.querySelectorAll("img").forEach((img) => {
-  img.onerror = function () {
-    console.error(`Failed to load image: ${this.src}`);
-    this.src = '/static/images/default_image.png';
-  };
+
+function setImageErrorHandling() {
+  document.querySelectorAll("img").forEach((img) => {
+    img.onerror = function () {
+      console.error(`Failed to load image: ${this.src}`);
+      this.src = '/static/images/default_image.png';
+    };
+  });
+}
+
+setImageErrorHandling();
+
+const observer = new MutationObserver(setImageErrorHandling);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
 });
+
+document.addEventListener('DOMContentLoaded', setImageErrorHandling);
   
