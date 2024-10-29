1abefcdb93b91fa4f9ffb4db0b414999644138d0
Kem Guianalan <guianalankem@gmail.com>
2024-10-30 00:43:25 +0800

    Load default image if image not found

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,10 @@
 // Load default image if image not found
 document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(() => {
-    document.querySelectorAll("img").forEach((img) => {
-      img.onerror = function () {
-        console.error(`Failed to load image: ${this.src}`);
-        this.src = '/static/images/default_image.png';
-      };
-    });
-  }, 5000);
+document.querySelectorAll("img").forEach((img) => {
+    img.onerror = function () {
+      console.error(`Failed to load image: ${this.src}`);
+      this.src = '/static/images/default_image.png';
+    };
+  });
 });
   
