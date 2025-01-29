194899e40fe7e36acf8ab5289f18a054a627679d
Kem Guianalan <guianalankem@gmail.com>
2025-01-29 22:59:55 +0800

    add job unsaving functionality; implement user jobs retrieval in the backend; enhance job saving logic with timestamps

--- careers/urls.py
+++ careers/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('careers/', views.careers, name='careers'),
     path('careers/<int:id>/', views.career_page, name='career_page'),
     path('careers/save_job/<int:id>/', views.save_job, name='save_job'),
+    path('careers/unsave_job/<int:id>/', views.unsave_job, name='unsave_job'),
+    path('careers/userjobs/', views.user_jobs, name='user_jobs'),
 ]
