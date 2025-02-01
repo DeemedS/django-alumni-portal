281d47f3ff044bf9c2068d0622cddc4108ba1eb1
Kem Guianalan <guianalankem@gmail.com>
2025-02-01 15:03:44 +0800

    add faculty dashboard and sidebar components; implement user greeting, navigation, and styling

--- faculty/urls.py
+++ faculty/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from articles import views
+from . import views as faculty_views
+from articles import views as article_views
 
 app_name = 'faculty'
 
 urlpatterns = [
 
-    path('faculty/articles/<slug:slug>/edit', views.edit_article, name='edit_article'),
-    path('update-order/', views.update_order, name='update_order'),
+    path('faculty/articles/<slug:slug>/edit', article_views.edit_article, name='edit_article'),
+    path('faculty/dashboard', faculty_views.faculty_dashboard, name='faculty_dashboard'),
+    path('faculty/logout/', faculty_views.faculty_logout, name='faculty_logout'),
+    path('update-order/', article_views.update_order, name='update_order'),
     
 ]
--- faculty/views.py
+++ faculty/views.py
@@ -1,5 +1,33 @@
-
+from django.shortcuts import redirect
+import requests
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib import messages
+from django.urls import reverse
+from django.contrib.messages import get_messages
+from django.contrib.auth import logout
 
-# Create your views here.
 
+@login_required(login_url='/faculty/')
+def faculty_dashboard(request):
+    if not request.user.is_staff or not request.user.is_active:
+        messages.error(request, "Access denied. You must be an active faculty member to proceed.")
+        
+        # Debug: Print stored messages before redirecting
+        storage = get_messages(request)
+        print("Messages before redirect:", list(storage))  # Check if message exists
+
+        return redirect(reverse('authentication:faculty'))
+    
+    context = {
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+    }
+
+    return render(request, 'faculty_dashboard.html', context)
+    
+
+def faculty_logout(request):
+    logout(request)
+    messages.success(request, "You have been logged out successfully.")
+    return redirect('/faculty') 
