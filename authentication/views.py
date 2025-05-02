a45c2f588adb303e8c89f761ba7431dcf92006b4
Kem Guianalan <guianalankem@gmail.com>
2025-05-02 09:18:19 +0800

    Remove unused import of reverse from views.py

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from django.urls import reverse
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth import get_user_model
