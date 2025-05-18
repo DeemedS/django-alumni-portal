73de95f02f62ae48c96d9507d21033bd18c9acd1
deemeds <guianalankem@gmail.com>
2025-05-18 13:51:58 +0800

    Add related alumni slider component and enhance event/news card layout

---
+++ users/static/js/related_alumni_slider_init.js
@@ -0,0 +1,18 @@
+
+var splide = new Splide( '.splide', {
+    perPage: 3,
+    perMove: 1,
+    autoplay: false,
+    speed: 1000,
+    arrows: true,
+    gap: '0.5rem',
+    pauseOnFocus: true,
+    mediaQuery: 'max',
+    pagination: false,
+    type: 'slide',
+  },
+   );
+  
+  
+  
+  splide.mount();
