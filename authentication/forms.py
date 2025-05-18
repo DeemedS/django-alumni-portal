bc88bd37cdcaf80501f9fa83a2278780742935f1
deemeds <guianalankem@gmail.com>
2025-05-18 23:26:45 +0800

    Add suffix field to user form and populate it in alumni edit template

--- authentication/forms.py
+++ authentication/forms.py
@@ -5,4 +5,4 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'middle_name', 'birthday', 'address', 'telephone', 'mobile', 'civil_status', 'sex']
+        fields = ['first_name', 'last_name', 'middle_name', 'suffix', 'birthday', 'address', 'telephone', 'mobile', 'civil_status', 'sex']
