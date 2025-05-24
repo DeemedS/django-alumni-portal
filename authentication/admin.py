907ac6433fa4c579f6d3189cbcabf44a04f0f839
Kem Guianalan <guianalankem@gmail.com>
2025-05-25 02:32:12 +0800

    Add UserSettings model and update profile image handling in User model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Course, Section
+from .models import UserSettings
 
 class UserAdmin(UserAdmin):
     model = User
     list_display = ('email', 'student_number', 'is_staff', 'is_active',)
     list_filter = ('course', 'section', 'school_year', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'student_number', 'first_name', 'last_name', 
-                        'middle_name', 'birthday', 'address', 'telephone', 'mobile', 'civil_status', 'sex', 'course', 'section', 'school_year',
+        (None, {'fields': ('email', 'password', 'student_number', 'profile_image', 'first_name', 'last_name', 
+                        'middle_name', 'suffix', 'birthday', 'address', 'telephone', 'mobile', 'civil_status', 'sex', 'course', 'section', 'school_year',
                         'education', 'licenses', 'certifications', 'work_experience',
                         'jobs', 'events', 
                         'email_verified')}),
@@ -23,6 +24,11 @@ class UserAdmin(UserAdmin):
     search_fields = ('email',)
     ordering = ('email',)
 
+class UserSettingsAdmin(admin.ModelAdmin):
+    list_display = ['default_profile_image']
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Course)
 admin.site.register(Section)
+admin.site.register(UserSettings)
+
