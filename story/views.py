b83cb46fa56c888c8decbb3db7a0ee47c47d7379
Sunday Padua <paduasunday@gmail.com>
2025-06-02 23:55:48 +0800

    Add story feature with templates, styles, and navigation integration

--- story/views.py
+++ story/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def story(request):
+    return render(request, 'story/story.html', {})
+
+def story_page(request):
+    return render(request, 'story/story_page.html', {})
