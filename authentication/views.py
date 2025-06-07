c3e65c6ceb0ad7cfb413da2c8d351298046ed788
deemeds <guianalankem@gmail.com>
2025-06-08 00:58:06 +0800

    Fix access and refresh token cookie settings for secure handling in user login

--- authentication/views.py
+++ authentication/views.py
@@ -57,8 +57,8 @@ def user_login(request):
             refresh = RefreshToken.for_user(user)
             access_token = AccessToken.for_user(user)
             response = redirect('/myaccount/')
-            response.set_cookie('access_token', str(access_token), httponly=True, samesite='Lax', secure=True)
-            response.set_cookie('refresh_token', str(access_token), httponly=True, samesite='Lax', secure=True)
+            response.set_cookie('access_token', str(access_token), httponly=True, secure=True)
+            response.set_cookie('refresh_token', str(refresh), httponly=True, secure=True)
             return response
         else:
             messages.error(request, 'Invalid email or password')
