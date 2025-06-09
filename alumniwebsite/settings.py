d5fd35bd17d0b1f8f71af4e376a7bf58b3e2cb40
Kem Guianalan <guianalankem@gmail.com>
2025-06-09 23:54:16 +0800

    Fix middleware import path for PermissionsPolicyMiddleware in settings

--- alumniwebsite/settings.py
+++ alumniwebsite/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
     'django_recaptcha',
     'fontawesomefree',
     'rest_framework',
-    'permissions_policy',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +75,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'permissions_policy.PermissionsPolicyMiddleware',
+    "django_permissions_policy.PermissionsPolicyMiddleware",
 ]
 
 ROOT_URLCONF = 'alumniwebsite.urls'
