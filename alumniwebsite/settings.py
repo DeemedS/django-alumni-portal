a56ceeb2be0046bca89012929f13e6ec6246fa5b
deemeds <guianalankem@gmail.com>
2025-06-16 01:34:58 +0800

    Revert "Refactor event and article filtering logic to improve error handling and logging"
    This reverts commit 6c7b5331f83b6d5ef51695377b0ade74bc518a38.

--- alumniwebsite/settings.py
+++ alumniwebsite/settings.py
@@ -239,6 +239,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10/min',
+    },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
