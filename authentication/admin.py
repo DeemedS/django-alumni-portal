e68ce1b1544b7f6b2c37ca5edde915edf427826c
deemeds <guianalankem@gmail.com>
2025-06-16 08:16:12 +0800

    Add custom UserChangeForm and update UserAdmin permissions handling
    - Introduced UserChangeForm to manage user fields dynamically.
    - Disabled 'is_superuser' field for non-superusers in the form.
    - Updated UserAdmin to conditionally display 'is_superuser' in permissions for superusers only.
    - Ensured only superusers can add users and modified save logic to prevent staff from setting 'is_superuser' to True.

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,11 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Course, Section
 from .models import UserSettings
+from django import forms
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Disable 'is_superuser' if user is not superuser
+        if not self.current_user.is_superuser:
+            self.fields['is_superuser'].disabled = True
 
 class UserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'student_number', 'is_staff', 'is_active',)
-    list_filter = ('course', 'section', 'year_graduated', 'is_staff', 'is_active',)
+
+    list_display = ('email', 'student_number', 'is_staff', 'is_active')
+    list_filter = ('course', 'section', 'year_graduated', 'is_staff', 'is_active')
+
     fieldsets = (
         (None, {'fields': (
             'email', 'password', 'student_number', 'profile_image', 'first_name', 'last_name',
@@ -22,15 +36,17 @@ class UserAdmin(UserAdmin):
             'fields': ('email', 'student_number', 'password1', 'password2', 'is_staff', 'is_active')
         }),
     )
+
     search_fields = ('email',)
     ordering = ('email',)
 
+    form = UserChangeForm  # use our custom form
+
     def get_fieldsets(self, request, obj=None):
-        # Convert tuple to list so we can modify
         fieldsets = list(self.fieldsets)
 
+        # Add is_superuser to Permissions only for superusers
         if request.user.is_superuser:
-            # Add is_superuser to Permissions fields
             for i, (name, options) in enumerate(fieldsets):
                 if name == 'Permissions':
                     fields = list(options.get('fields', ()))
@@ -38,8 +54,22 @@ class UserAdmin(UserAdmin):
                         fields.append('is_superuser')
                     fieldsets[i] = (name, {'fields': tuple(fields)})
                     break
-
         return fieldsets
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        # Attach current user to form instance so we can disable fields dynamically
+        form.current_user = request.user
+        return form
+
+    def has_add_permission(self, request):
+        # Only superusers can add users (including superusers)
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        # Staff can change all users but cannot make themselves superuser in form
+        # (Field disabling handled in form)
+        return True
 
 class UserSettingsAdmin(admin.ModelAdmin):
     list_display = ['default_profile_image']
--- authentication/forms.py
+++ authentication/forms.py
@@ -6,3 +6,12 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'middle_name', 'suffix', 'birthday', 'address', 'telephone', 'mobile', 'civil_status', 'sex']
+
+def has_add_permission(self, request):
+    return request.user.is_superuser or request.user.is_staff
+
+def save_model(self, request, obj, form, change):
+    if not request.user.is_superuser:
+        # Ensure staff cannot set is_superuser True
+        obj.is_superuser = False
+    super().save_model(request, obj, form, change)
