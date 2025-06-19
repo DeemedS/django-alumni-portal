41da3eaf48d1d76d25e20e7f81558e48b5beef11
Kem Guianalan <guianalankem@gmail.com>
2025-06-19 20:29:15 +0800

    Remove unused authentication token handling from views

--- about/views.py
+++ about/views.py
@@ -7,8 +7,6 @@ from alumniwebsite.forms import FormWithCaptcha
 # Create your views here.
 
 def about(request):
-    access_token = request.COOKIES.get('access_token')
-    refresh_token = request.COOKIES.get('refresh_token')
     websettings = WebsiteSettings.objects.first()
 
     officials_by_position = {}
@@ -18,22 +16,11 @@ def about(request):
         official = Official.objects.filter(position=pos_key).first()
         officials_by_position[slug_key] = official
 
-    is_authenticated = False
-
-    if access_token and refresh_token:
-        # Here you might want to validate the tokens or perform some action
-        api_url = f"{settings.API_TOKEN_URL}/token/verify/"
-        data = {'token': access_token}
-        response = requests.post(api_url, data=data)
-
-        if response.status_code == 200:
-            is_authenticated = True
-            
+        
     context = {
         'settings': websettings,
         'form' : FormWithCaptcha(),
         "RECAPTCHA_PUBLIC_KEY": settings.RECAPTCHA_PUBLIC_KEY,
-        'is_authenticated': is_authenticated,
         'officials': officials_by_position
     }
 
--- homepage/views.py
+++ homepage/views.py
@@ -7,9 +7,6 @@ from django.conf import settings
 from alumniwebsite.forms import FormWithCaptcha
 
 def home(request):
-    access_token = request.COOKIES.get('access_token')
-    refresh_token = request.COOKIES.get('refresh_token')
-
     form = FormWithCaptcha()
     
     featured_article = Article.objects.filter(featured=True, is_active=True).first()
@@ -17,22 +14,11 @@ def home(request):
     event_article = Event.objects.filter(is_active=True).order_by('-date').first()
     websettings = WebsiteSettings.objects.first()
 
-    is_authenticated = False
-
-    if access_token and refresh_token:
-        api_url = f"{settings.API_TOKEN_URL}/token/verify/"
-        data = {'token': access_token}
-        response = requests.post(api_url, data=data)
-
-        if response.status_code == 200:
-            is_authenticated = True
-
     context = {
         'featured_article': featured_article,
         'news_articles': news_articles,
         'event_article': event_article,
         'school_abv': settings.SCHOOL_ABV,
-        'is_authenticated': is_authenticated,
         'settings': websettings,
         "form": form,
         "RECAPTCHA_PUBLIC_KEY": settings.RECAPTCHA_PUBLIC_KEY
